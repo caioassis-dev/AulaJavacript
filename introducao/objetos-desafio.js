fe7b929585fa732676620e2482585c1ce1fc72c1
Caio Freire Lima de Assis <CS376562@MinhaTI.com.br>
2022-09-24 13:32:40 -0300

    Adicionando itens ao loader e resolucao do desafio de criar objetos

--- introducao/objetos-desafio.js
+++ introducao/objetos-desafio.js
@@ -8,5 +8,27 @@
    peso          comer()
    altura        beber()
    sexo          falar()
-   cor           andar()
+                 andar()
 */
+
+const pessoa = {
+    nome: "Maria", 
+    peso: 75, 
+    altura: 1.65, 
+    sexo: "F", 
+    dormir: function() {
+        return "dormindo"
+    }, 
+    comer: function() {
+        return "comendo"
+    },
+    beber: function() {
+        return "bebendo"
+    },
+    falar: function() {
+        return "falando"
+    },
+    andar: function() {
+        return "andando"
+    },
+};
